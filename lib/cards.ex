bd0862645334c782a685afa9fe50f3d54d92918e
Gustavo Santos <gustavoms97@hotmail.com>
2020-03-01 04:04:22 -0300

    Adicionado método para criar a mão, utilizando pipe operator para realizar varios metodos em seguida. Foi adicionada a dependencia ex_docs para gerar documentação de módulos e funções, que foi adicionado, com a parte de exemplos tambem. Foram criados testes de métodos e modulos, que são feitos automaticamentes utilizando os 'Examples' da documentação + os case tests.

--- lib/cards.ex
+++ lib/cards.ex
@@ -3,6 +3,13 @@ defmodule Cards do
   # É possivel ter varios metodos com o mesmo nome, apenas com o numero de
   # parametros diferentes.
 
+  @moduledoc """
+    Provides methods for creating and handling a deck of cards.
+  """
+
+  @doc """
+    Returns a list of strings representing a deck of playing cards
+  """
   def create_deck do
     values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven"]
     suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
@@ -22,25 +29,54 @@ defmodule Cards do
     end
   end
 
+  @doc """
+  """
   def shuffle(deck) do
     Enum.shuffle(deck)
   end
 
   # Metodos com "?" nao mudam em funcionalidade, é apenas uma boa pratica
   # para facilitar o entendimento dos desenvolvedores.
+  @doc """
+    Determines wether a deck contains a given card
+
+  ## Examples
+
+      iex> deck = Cards.create_deck
+      iex> Cards.contains?(deck, "Ace of Spades")
+      true
+
+  """
   def contains?(deck, card) do
     Enum.member?(deck, card)
   end
 
+  @doc """
+    Divides a deck into a hand and the remainder of the deck.
+    The `hand_size` argument indicates how many cards should
+    be in the hand
+
+  ## Examples
+
+      iex> deck = Cards.create_deck
+      iex> {hand, deck} = Cards.deal(deck,1)
+      iex> hand
+      ["Ace of Spades"]
+
+  """
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
   end
 
+  @doc """
+  """
   def save(deck, filename) do
     binary = :erlang.term_to_binary(deck)
     File.write(filename, binary)
   end
 
+  @doc """
+  """
   def load(filename) do
     # {status, binary} = File.read(filename)
     # case status do
@@ -59,5 +95,21 @@ defmodule Cards do
       {:ok, binary} -> :erlang.binary_to_term(binary)
       {:error, _reason} -> "That file does not exist"
     end
+  end
+
+  @doc """
+  """
+  def create_hand(hand_size) do
+    # deck = Cards.create_deck
+    # deck = Cards.shuffle(deck)
+    # hand = Cards.deal(deck, hand_size)
+
+    {hand, _rest} =
+      Cards.create_deck()
+      |> Cards.shuffle()
+      # Pipe operator funciona apenas com o primeiro parametro(s) em ordem
+      |> Cards.deal(hand_size)
+
+    hand
   end
 end
